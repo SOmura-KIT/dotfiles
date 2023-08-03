685e5d03f58efe4b0ab5f4d80de58c118046e7dc
Yuto Somura <c1100921@planet.kanazawa-it.ac.jp>
2023-08-03 22:48:07 +0900

    :refacter:

--- nvim/plugins/lualine.lua
+++ nvim/plugins/lualine.lua
@@ -1,31 +1,3 @@
---[[
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
-  },
-  section = {},
-  inactive_section = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
-}
-]]
-
 require('lualine').setup {
   options = {
     icons_enabled = true,
