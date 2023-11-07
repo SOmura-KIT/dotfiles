1dd2a762bcc6fe87ceaf50af5578092e39eb7bfb
Yuto Somura <c1100921@planet.kanazawa-it.ac.jp>
2023-11-07 11:10:16 +0900

    :add: リンク作成用のスクリプトを作成

---
+++ install.sh
@@ -0,0 +1,9 @@
+# *** dotfilesディレクトリをホームディレクトリ上にして実行すること ***
+# To-Do: ディレクトリ位置を意識しなくてもいいよう変更
+ln -s $HOME/dotfiles/alacrity/  $HOME/.config/.
+ln -s $HOME/dotfiles/i3/        $HOME/.config/.
+ln -s $HOME/dotfiles/nvim/      $HOME/.config/.
+ln -s $HOME/dotfiles/polybar/   $HOME/.config/.
+ln -s $HOME/dotfiles/picom.conf $HOME/.config/.
+ln -s $HOME/dotfiles/.tmux.conf $HOME/.
+ln -s $HOME/dotfiles/.zshrc     $HOME/.
