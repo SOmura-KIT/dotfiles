5b1a4bd8117318028a7652a4b204e56e08bf9315
Yuto Somura <c1100921@planet.kanazawa-it.ac.jp>
2023-11-08 10:06:22 +0900

    :change: polybarをpolybar-themeに変更

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 ln -s $HOME/dotfiles/alacrity/  $HOME/.config/.
 ln -s $HOME/dotfiles/i3/        $HOME/.config/.
 ln -s $HOME/dotfiles/nvim/      $HOME/.config/.
-ln -s $HOME/dotfiles/polybar/   $HOME/.config/.
 ln -s $HOME/dotfiles/picom.conf $HOME/.config/.
 ln -s $HOME/dotfiles/.tmux.conf $HOME/.
 ln -s $HOME/dotfiles/.zshrc     $HOME/.
+$ git clone --depth=1 https://github.com/adi1090x/polybar-themes.git $HOME/dotfiles/.
